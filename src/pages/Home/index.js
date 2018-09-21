6f10540b208cef0135a80c164b590cc5bb69886d
amiel.suarez@nuworks.ph <amiel.suarez@nuworks.ph>
2018-09-21 18:13:22 +0800

    LMAO

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,6 +8,30 @@ class Home extends Component {
 
 	render() {
 		return (<div className="page_home">
+			<div className="container">
+				<b id="score_container">0</b>
+				<b id="score_assist_container">0</b>
+				<b id="score_average_container">0%</b>
+				<div className="staff">
+					<div className="lines"></div>
+					<div className="lines"></div>
+					<div className="lines"></div>
+					<div className="lines"></div>
+					<div className="note"></div>
+
+					<div id="note_container">
+						
+					</div>
+
+					<div id="note_toastr_container">
+						<div className="note_toastr">100%</div>
+					</div>
+				</div>
+
+				<div id="correct_display">
+					<div id="correct_bar"></div>
+				</div>
+			</div>
 			{this.props.note.name}
 		</div>)
 	}
